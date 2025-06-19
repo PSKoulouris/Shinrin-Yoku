46a9802d4897831f6f8c8d223fe37f5e71499453
PSKoulouris <philippe.koulouris@gmail.com>
2025-06-19 22:32:44 +0200

    second commit

--- src/app/api-plants/api-plants.ts
+++ src/app/api-plants/api-plants.ts
@@ -12,14 +12,11 @@ import { HttpClient } from '@angular/common/http';
 export class ApiPlants {
 
   
-   private apiURL = 'https://weather-api-h0fdwt.5sc6y6-4.usa-e2.cloudhub.io/forecast'
+   private apiURL = 'data/plantsSummary.json'
 
-  forecastList: {
-    date:string;
-    description:string;
-    tempMax:number;
-    tempMin:number;
-    logo:string;
+  plantsList: {
+   id: number,
+   image: string
   }[] = [];
 
   // j'install le téléphone chez moi
@@ -28,18 +25,15 @@ export class ApiPlants {
 
 
   // fonction appel vers API
-  getForecastData(){
+  getPlantsData(){
     this.http.get(this.apiURL).subscribe({
       next:(data:any) =>{
 
         console.log(data);
 
-        this.forecastList = data["data"].map((day:any) => ({
-          date: day["datetime"],
-          description: day["weather"]["description"],
-          tempMin: day["app_min_temp"],
-          tempMax: day["app_max_temp"],
-          logo: day["weather"]["icon"]
+        this.plantsList = data["data"].map((plant:any) => ({
+           id: plant.id,
+          image: plant.default_image?.small_url
         }));
       }
     })
@@ -48,7 +42,7 @@ export class ApiPlants {
 
   // Lancer la function dans j'ouvre le composant
   ngOnInit(){
-    this.getForecastData();
+    this.getPlantsData();
   }
 
 }
