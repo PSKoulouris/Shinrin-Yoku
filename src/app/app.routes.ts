6bcabdf848d7a01c8e1856f83a764197c337ccf2
PSKoulouris <philippe.koulouris@gmail.com>
2025-06-21 11:24:55 +0200

    Commit CSS tailwind installation before error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Details } from './details/details';
 import { ApiPlants } from './api-plants/api-plants';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-    { path: '', component: ApiPlants },
+    { path: '', component: Home},
+    { path: 'plants', component: ApiPlants },
     { path: 'detail/:id', component: Details }
 ];
