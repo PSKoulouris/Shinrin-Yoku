254bd2c8a0dedf3e067d18f87162ce1c5337698a
PSKoulouris <philippe.koulouris@gmail.com>
2025-06-23 01:36:11 +0200

    css broken

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { ApiPlants } from './api-plants/api-plants';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
@@ -8,8 +8,18 @@ import { Footer } from './footer/footer';
   selector: 'app-root',
   imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']  // note: it's 'styleUrls', plural
 })
-export class App {
+export class App implements OnInit {
   protected title = 'Shinrin-yoku';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
