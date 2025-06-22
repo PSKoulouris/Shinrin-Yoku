2722352355eda8a0c1e5b45f5a3695096c5dac31
PSKoulouris <philippe.koulouris@gmail.com>
2025-06-22 22:07:21 +0200

    change detail pages

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-details',
@@ -15,7 +16,7 @@ export class Details {
   plantData: any;
   private apiURL = 'data/data_details.json';
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient,  private location: Location) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -26,8 +27,21 @@ export class Details {
 
   loadPlantData() {
     this.http.get(this.apiURL).subscribe((data: any) => {
-      this.plantData = data.data.find((p: any) => p.id === this.id);
+      const plant = data.data.find((p: any) => p.id === this.id);
+      if (plant && plant.common_name) {
+        plant.common_name = this.capitalizeFirstLetter(plant.common_name);
+      }
+      this.plantData = plant;
     });
   }
+
+  private capitalizeFirstLetter(text: string): string {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  }
+
+  //Return button from detail to previous page:
+  goBack(): void {
+    this.location.back();
 }
 
+}
