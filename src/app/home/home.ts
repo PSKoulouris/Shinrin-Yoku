b0e72da324160b43c5ab7c74d720f53a91cae40d
PSKoulouris <philippe.koulouris@gmail.com>
2025-06-24 12:11:47 +0200

    forms udatgex

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,10 @@
 import { Component, ViewChild, ElementRef, inject, ChangeDetectorRef } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
   // Optional: changeDetection: ChangeDetectionStrategy.OnPush
@@ -24,33 +23,6 @@ export class Home {
     this.contactSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
   }
 
-  //Form submission
 
-  formData = {
-    name: '',
-    email: '',
-    message: ''
-  };
 
-  submissionSuccess = false;
-
-  onSubmit(form: NgForm): void {
-    if (form.valid) {
-      console.log('Form submitted', this.formData);
-      this.submissionSuccess = true;
-
-      form.resetForm();
-
-      setTimeout(() => {
-        this.submissionSuccess = false;
-      }, 5000);
-    } else {
-      // Mark all controls touched to trigger validation messages immediately
-      form.form.markAllAsTouched();
-      this.submissionSuccess = false;
-
-      // Manually trigger change detection (important if OnPush is used)
-      this.cdr.detectChanges();
-    }
   }
-}
